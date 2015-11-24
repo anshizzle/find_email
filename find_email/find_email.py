e1ef364800255b26b12c7158dbe8c089aa56371c
AJ <mosquito242@gmail.com>
2015-11-24 09:41:40 -0800

    read file now built into find_email utility

--- find_email/find_email.py
+++ find_email/find_email.py
@@ -1,6 +1,7 @@
 from validate_email import validate_email
 import sys
 import os
+import csv
 
 # Holds a list of potential configurations for an email address
 # ie {first_name}{last_initial} would be anshulj for anshul jain
@@ -40,18 +41,53 @@ def find_email(first_name, last_name, company_domain):
     #print "email not valid"
   return None
 
+def read_from_file(input, output):
+  with open(input, 'rb') as csvfile:
+    leads = csv.reader(csvfile, delimiter=',')
+    with open(output, 'wb') as csvof:
+      of = csv.writer(csvof, delimiter=',')
+      for row in leads:
+        first_name, last_name = row[1].split()
+        domain = row[4]
+        row[3] = find_email(first_name, last_name, domain)
+        of.writerow(row)
+        print first_name + " " + last_name + " from " + row[0] + " found"
+
+  print "Done getting emails"
+
 def main():
-  if len(sys.argv) < 4:
+  load_configs()
+
+  if len(sys.argv) == 3:
+    read_from_file(sys.argv[1], sys.argv[2])
+    sys.exit(0)
+  elif len(sys.argv) < 4:
     print "ERROR: find_email expects 3 arguments, only " + str(len(sys.argv)-1) + " received"
     sys.exit(1)
 
-  load_configs()
   email = find_email(sys.argv[1], sys.argv[2], sys.argv[3])
 
   if email:
     print email
   else:
     print "email not found"
+
+
+# if __name__ == "__main__":
+
+#   if len(sys.argv) != 3:
+#     print "ERROR: wrong number of args"
+#     sys.exit(1)
+
+#   load_configs()
+#   file_name = sys.argv[1]
+#   output_file_name = sys.argv[2]
+
+
+
+
+
+
 
 if __name__ == "__main__":
   main()
